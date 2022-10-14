6af181a0f0034ea36726d0c01a729baf512107f0
Darkpuzzy <zhirkov_lol@mail.ru>
2022-10-14 18:54:57 +0300

    init

---
+++ apiord/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Car(models.Model):
+    pass
+
+
+class Colour(models.Model):
+    pass
+
+
+class CarsModel(models.Model):
+    pass
+
+
+class Order(models.Model):
+    pass
